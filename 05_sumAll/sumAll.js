faa0bdfc21de69c5f77e9f935ecf82ffdd376f24
albert-881 <quinteroalberto88@gmail.com>
2024-07-23 16:42:11 -0500

    finished task 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,30 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    let i = num1;
+    
+    if((num1 < 0 || num2 < 0 || !Number.isInteger(num1)) || !Number.isInteger(num2)) {
+        return 'ERROR';
+    }
 
+    else if(num1 < num2){
+
+        while(i <= num2){
+            sum += i;
+            i++;
+        }
+    console.log(sum);
+    return sum;
+}
+    else if(num1 > num2){
+
+        while(i >= num2){
+            sum += i;
+            i--;
+        }
+        console.log(sum);
+        return sum;
+    }
+    
 };
 
 // Do not edit below this line
