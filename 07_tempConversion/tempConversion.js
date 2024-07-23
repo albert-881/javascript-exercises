cb3cb576318702fdb3f7e9797ceb0198b24ead3c
albert-881 <quinteroalberto88@gmail.com>
2024-07-23 17:20:01 -0500

    finished task 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  
+  let cel = Math.round((fahrenheit - 32) * (5 / 9) * 10) / 10;
+  return cel;
+  
+  
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  
+  let fah = Math.round(((celsius * 9) / 5 + 32) * 10) / 10;
+  return fah;
+  
 };
 
 // Do not edit below this line
