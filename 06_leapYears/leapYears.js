2d6a74c122e51d2d5254faf525bf81288c5a375a
albert-881 <quinteroalberto88@gmail.com>
2024-07-23 16:59:43 -0500

    finished task 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,18 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (Number.isInteger(year / 4)){
+        if (Number.isInteger(year / 100) && Number.isInteger(year / 400)){
+            return true;
+        }
+        else if (!Number.isInteger(year / 100)){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    else{
+        return false;
+    }
 };
 
 // Do not edit below this line
