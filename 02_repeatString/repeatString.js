2cc4194ee4f3fa7043ec2662e5fa05b99c7a08d7
albert-881 <quinteroalberto88@gmail.com>
2024-07-23 14:59:12 -0500

    finished task 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
-
-};
+const repeatString = function(word,num) {
+    if (num < 0){
+        return 'ERROR'
+    }
+    let j = 0;
+    let str1 = ""
+    while (j < num){
+       str1+=word;
+       j++;
+    }
+    console.log(str1);
+    return str1;
+}
 
 // Do not edit below this line
 module.exports = repeatString;
