3b3e0ae98b7dc0dfdc901ecca16894981c48db5a
albert-881 <quinteroalberto88@gmail.com>
2024-07-23 15:14:25 -0500

    completed task 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let w = word.split('');
+    let rword = w.reverse();
+    let reversedWord = rword.join('');
+    return reversedWord;
 };
 
 // Do not edit below this line
