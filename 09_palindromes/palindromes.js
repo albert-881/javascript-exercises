623650c42a47aace7abfcade9bac1fd08846012b
albert-881 <quinteroalberto88@gmail.com>
2024-07-28 16:29:25 -0500

    finished 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz0123456789'
+    word = word.toLowerCase();
+    let letArr = word.split('');
+    let fil = letArr.filter((letter) => alphabet.includes(letter));
+    let fin = fil.join('')
+    let final = fil.reverse().join('');
+    
+     return final === fin;
+    //return singles.filter((letter) => )
 };
 
 // Do not edit below this line
